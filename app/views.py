8d20473557ca727aad4620e019407cc0e9757aeb
Ronae Johnson <ronae2004@gmail.com>
2022-02-07 20:35:00 -0500

    UI_built

--- app/views.py
+++ app/views.py
@@ -24,6 +24,11 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    """Render website's home page."""
+    return render_template('profile.html')
+
 
 ###
 # The functions below should be applicable to all Flask apps.
