b5139de615c4c4cb5f6eabf5cda6f56d1f2a2989
aabishuaa <abishuasjohnson@gmail.com>
2025-02-26 19:22:11 -0500

    Profile route added to views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime  # Added for date handling
 
 ###
 # Routing for your application.
@@ -18,9 +18,31 @@ def about():
     return render_template('about.html', name="Mary Jane")
 
 
-###
-# The functions below should be applicable to all Flask apps.
-###
+@app.route('/profile')
+def profile():
+    """Render the fake profile page."""
+    date_joined = datetime.date(2019, 2, 7)
+    formatted_date = format_date_joined(date_joined)
+    
+    profile_data = {
+        'full_name': 'Abishua S. Johnson',           
+        'username': 'abishua',             
+        'location': 'Kingston, Jamaica',       
+        'date_joined': formatted_date,
+        'bio': 'I am a final year Computer Science major who is passionate about Artificial Intelligence and Machine Learning.',
+        'posts': 10,
+        'followers': 7560789,
+        'following': 0,
+        'image': 'abishua.jpg'    
+    }
+    
+    return render_template('profile.html', profile=profile_data)
+
+
+def format_date_joined(date_joined):
+    """Formats a date object into 'Month, Year' format."""
+    return date_joined.strftime("%B, %Y")
+
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
@@ -32,9 +54,8 @@ def send_text_file(file_name):
 @app.after_request
 def add_header(response):
     """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
+    Add headers to force the latest IE rendering engine or Chrome Frame,
+    and also tell the browser not to cache the rendered page.
     """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
